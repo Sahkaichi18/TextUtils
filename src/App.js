1a4c881317bccb1aa7234616c527f5992b01ac51
Sahkaichi18 <sahilbhosale1804@gmail.com>
2025-08-01 19:44:38 +0530

    Quick update

--- src/App.js
+++ src/App.js
@@ -26,11 +26,19 @@ function App() {
       setMode ('dark');
       document.body.style.backgroundColor = '#070e22';
       showAlert("Dark mode has been enabled", "success : ");
+      document.title = 'TextUtils - Dark Mode';
+      // setInterval(() => {
+      //   document.title = 'TextUtils is Amazing !';
+      // }, 2500);
+      // setInterval(() => {
+      //   document.title = 'Install TextUtils !';
+      // }, 1500);
     }
     else{
       setMode ('light');
       document.body.style.backgroundColor = 'white';
       showAlert("Light mode has been enabled", "success : ");
+      document.title = 'TextUtils - Light Mode';
     }
     
   }
