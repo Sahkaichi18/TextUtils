bfe798c4d4fec5622f31f8c5477026580ede224c
Sahkaichi18 <sahilbhosale1804@gmail.com>
2025-08-04 21:38:30 +0530

    Quick update

--- src/components/About.js
+++ src/components/About.js
@@ -34,7 +34,7 @@ const[btnText, setBtnText] = useState("Enable Dark mode")
             <div className="accordion-item" style = {myStyle}>
                 <h2 className="accordion-header">
                 <button className="accordion-button" style = {myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                    Analyze your text
+                   <strong>Analyze your text</strong> 
                 </button>
                 </h2>
                 <div id="collapseOne" className="accordion-collapse collapse show" data-bs-parent="#accordionExample">
@@ -46,7 +46,7 @@ const[btnText, setBtnText] = useState("Enable Dark mode")
             <div className="accordion-item"style = {myStyle}>
                 <h2 className="accordion-header">
                 <button className="accordion-button collapsed" style = {myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                    Free to use
+                   <strong>Free to use</strong> 
                 </button>
                 </h2>
                 <div id="collapseTwo" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
@@ -58,7 +58,7 @@ const[btnText, setBtnText] = useState("Enable Dark mode")
             <div className="accordion-item" style = {myStyle}>
                 <h2 className="accordion-header">
                 <button className="accordion-button collapsed" style = {myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                    Accordion Item #3
+                  <strong>Browser compatible</strong>
                 </button>
                 </h2>
                 <div id="collapseThree" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
